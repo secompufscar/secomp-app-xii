ef4c1a004d317adcdd5d8a208fd87789946c6b63
lucasac00 <115199247+lucasac00@users.noreply.github.com>
2024-05-17 17:12:05 -0300

    Update tailwind.config.js
    Adicionei o verde utilizado no figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
           400: "#B6ECD6",
           500: "#9FE6CA",
           600: "#69D8AC",
-          700: "#33C289",
+          700: "#51B68D", //Verde utilizado no figma
           800: "#2AA272",
           900: "#1F7553",
           950: "#16553C"
