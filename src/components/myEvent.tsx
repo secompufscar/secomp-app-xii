17b21ac86cfec349efc04f3ccd75e24029a96eb0
lucasac00 <luc.fast2004@gmail.com>
2024-06-21 09:45:12 -0300

    fix: estilização do componente myEvent

--- src/components/myEvent.tsx
+++ src/components/myEvent.tsx
@@ -3,17 +3,20 @@ import {View, Text, Button, TouchableHighlight, TouchableOpacity} from 'react-na
 import { ScheduleItemProps } from '../entities/schedule-item';
 import { Feather } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import {Platform, StyleSheet} from "react-native";
+
+const isIos: Boolean = Platform.OS === 'ios'
 
 type ScheduleItemComponentProps = {
     scheduleItem: ScheduleItemProps;
-    onClick: () => void;
+    onClick?: () => void;
 }
 
 // Componente de ítens do cronogramam, pode ser mudado depois para aceitar mais informações
 export default function MyEvent({scheduleItem, onClick}: ScheduleItemComponentProps) {
     return (
         <View>
-            <View className='flex flex-col justify-start w-[156] rounded-md border border-green-700'>
+            <View className='flex flex-col justify-start w-[156] bg-[#FFFFFF] rounded-2xl' style={(isIos) ? [styles.shadowProp] : [styles.elevation]}>
                 <View className="p-4">
                     <View className='pb-2'>
                         <Text className='text-green-700 text-xl font-bold'>{scheduleItem.hour}</Text>
@@ -33,3 +36,16 @@ export default function MyEvent({scheduleItem, onClick}: ScheduleItemComponentPr
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    shadowProp: {
+        shadowColor: '#171717',
+        shadowOffset: {width: -1, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+    elevation: {
+        elevation: 5,
+        shadowColor: '#171717',
+    },
+})
