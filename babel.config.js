5bf125512e078c9d3aebb218f4da00388697b269
Enzo Murayama <enzomurayama@gmail.com>
2025-05-14 15:46:31 -0300

    chore: update dependencies in package.json

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      "@babel/plugin-transform-optional-catch-binding",
+      "@babel/plugin-transform-numeric-separator",
+      "@babel/plugin-transform-nullish-coalescing-operator",
+      "@babel/plugin-transform-class-properties",
+      "@babel/plugin-transform-logical-assignment-operators",
+      "@babel/plugin-transform-optional-chaining",
+      "@babel/plugin-transform-async-generator-functions",
+      "@babel/plugin-transform-object-rest-spread",
+    ],
   };
 };
